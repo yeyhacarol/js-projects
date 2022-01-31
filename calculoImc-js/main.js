d32e12b4eedde94f1d3a26f7b72fc7b3b6f6188d
Lucas Delmondes <carolinav.chavesilva@gmail.com>
2022-01-30 23:21:26 -0300

    fazendo alteração da cor das palavras em determinado caso

--- calculoImc-js/main.js
+++ calculoImc-js/main.js
@@ -10,11 +10,12 @@ function calcularImc(altura, peso) {
 }
 
 function estado(imc) {
-    let texto;
+    let texto = "";
+
     if (imc < 18.5) {
         texto = " e você está abaixo do peso.";
     } else if (imc > 18.5 && imc < 24.9) {
-        texto = " e você está no peso ideal. Parabéns!";
+        texto = " e você está no peso ideal. Parabéns!"
     } else if (imc > 25 && imc < 29.9) {
         texto = " e você está acima do peso. Cuidado!";
     } else if (imc > 30 && imc < 34.9) {
@@ -23,7 +24,7 @@ function estado(imc) {
         texto = " e você está com obesidade grau II.";
     } else {
         texto = " e você está com  obesidade severa.";
-    } 
+    }
 
     return texto;
 }
@@ -34,21 +35,41 @@ function exibirResultados() {
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
-    
+
     /* se os campos forem válidos definimos que a variável imc recebe o método calcularImc, e a variável texto recebe, consequentemente, 
         o estado  setado no método de mesmo nome e por fim, atribuirá os valores na caixa de texto que exibe os resultados*/
-    if (camposValidos()) { 
+    if (camposValidos()) {
         const imc = calcularImc(altura, peso);
-        
+
         let texto = estado(imc);
         //'.toFixed()' para definir a quantidade de casas decimais 
-        resultado.textContent = `${nome} seu IMC é ${imc.toFixed(2)} ${texto}`;
+        resultado.textContent = `${nome} seu IMC é ${imc.toFixed(2)}${texto}`;
+        verificaPalavra(resultado);
     } else {
         resultado.textContent = "Preencha todos os campos!";
     }
 }
 
+//método que altera as cor das palavras
+function verificaPalavra(resultado) {
+    //criando um array com cada uma das palavras 
+    const palavras = resultado.textContent.split(" ");
+    //limpando a caixa de texto que exibe os resultados fornecidos através do formulário para que texto não fique duplicado
+    resultado.innerHTML = "";
+
+    palavras.forEach((palavra) => {
+        //adicionando um span filho para cada palavra
+        const span = resultado.appendChild(document.createElement('span'));
+        //adicionando todas as palavras do formulário na caixa de texto 
+        span.textContent = palavra + ' ';
+        //verificações para agregar cores as palavras correspondentes
+        if (palavra === 'Parabéns!') span.classList.add('verde');
+        if (palavra === 'Cuidado!') span.classList.add('vermelho');
+    });
+};
+
 /*para capturarmos o botão, do qual a primeira parte antes do primeiro ponto 
     representa o elemento html "botão", em seguida definimos a captura do "click" no botão através do método de listener.
     callBack seria uma função do qual podemos criar/ser existente, representada pelo "exibirResultados".*/
 document.getElementById('calcular').addEventListener('click', /*callBack*/ exibirResultados);
+div.addEventListener("blur", verificaPalavra);
